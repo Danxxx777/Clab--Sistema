e47f522fc1fd321a458be093003bce0d58e9a6c3
BYRON DANIEL LOOR MENDOZA <bloorm2@msuteq.edu.ec>
2026-01-07 23:46:33 -0500

    init: spring boot backend

---
+++ backend/Clab-backend/src/test/java/com/clab/clabbackend/ClabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clab.clabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
