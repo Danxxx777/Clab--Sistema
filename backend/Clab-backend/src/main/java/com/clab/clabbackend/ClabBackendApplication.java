9dfbd27c4b767edb3409540c740b7861e27d84a9
BYRON DANIEL LOOR MENDOZA <bloorm2@msuteq.edu.ec>
2026-01-12 22:44:40 -0500

    Estructura correcta: backend/clab-backend y frontend

--- backend/Clab-backend/src/main/java/com/clab/clabbackend/ClabBackendApplication.java
+++ backend/Clab-backend/src/main/java/com/clab/clabbackend/ClabBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClabBackendApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ClabBackendApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ClabBackendApplication.class, args);
+	}
 
 }
