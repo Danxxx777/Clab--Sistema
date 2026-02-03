7c830dc6d1fb2562aeccfd2a482981ebb55e8b54
leoleo <lzambranot6@uteq.edu.ec>
2026-02-03 11:48:53 -0500

    CRUD de Sedes, aunque faltaria el delete, pero pa despues, ahora toca la gestion de usuarios :c

---
+++ frontend/clab-frontend/src/app/interfaces/Sedes.model.ts
@@ -0,0 +1,8 @@
+export interface Sedes {
+  idSede?: number;
+  nombre: string;
+  direccion: string;
+  telefono: string;
+  email: string;
+  estado: string;
+}
---
+++ frontend/clab-frontend/src/app/services/sede.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Sedes} from '../interfaces/Sedes.model';
+
+@Injectable({ providedIn: 'root' })
+export class SedeService {
+
+  private apiUrl = 'http://localhost:8080/sedes';
+
+  constructor(private http: HttpClient) {}
+
+  listar() {
+    return this.http.get<any[]>(`${this.apiUrl}/listar`);
+  }
+
+  crear(sede: Sedes) {
+    return this.http.post<Sedes>(
+      `${this.apiUrl}/crear`,
+      sede
+    );
+  }
+
+  editar(id: number, sede: Sedes) {
+    return this.http.put<Sedes>(
+      `${this.apiUrl}/actualizar/${id}`,
+      sede
+    );
+  }
+
+}
