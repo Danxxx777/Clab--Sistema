db9a1265a6b6bf9eb23ea5a809ff4f23487c9530
ameraj2-bit <ameraj2@uteq.edu.ec>
2026-02-09 17:18:57 -0500

    Lo del módulo equipos diooooooo

---
+++ backend/Clab-backend/src/main/java/com/clab/clabbackend/controller/EquipoController.java
@@ -0,0 +1,66 @@
+package com.clab.clabbackend.controller;
+
+import com.clab.clabbackend.dto.EquipoDTO;
+import com.clab.clabbackend.entities.Equipo;
+import com.clab.clabbackend.services.EquipoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/equipos")
+@RequiredArgsConstructor
+public class EquipoController {
+
+    private final EquipoService equipoService;
+
+    @GetMapping
+    public ResponseEntity<List<Equipo>> listar() {
+        return ResponseEntity.ok(equipoService.listar());
+    }
+
+    @PostMapping
+    public ResponseEntity<Equipo> crear(@RequestBody EquipoDTO dto) {
+        Equipo equipo = equipoService.crear(
+                dto.getNumeroSerie(),
+                dto.getNombreEquipo(),
+                dto.getMarca(),
+                dto.getModelo(),
+                dto.getTipoEquipo(),
+                dto.getEstado(),
+                dto.getLaboratorio(),
+                dto.getUbicacionFisica(),
+                dto.getFechaAdquisicion()
+        );
+        return ResponseEntity.ok(equipo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Equipo> editar(
+            @PathVariable Integer id,
+            @RequestBody EquipoDTO dto
+    ) {
+        Equipo equipo = equipoService.editar(
+                id,
+                dto.getNumeroSerie(),
+                dto.getNombreEquipo(),
+                dto.getMarca(),
+                dto.getModelo(),
+                dto.getTipoEquipo(),
+                dto.getEstado(),
+                dto.getLaboratorio(),
+                dto.getUbicacionFisica(),
+                dto.getFechaAdquisicion()
+        );
+        return ResponseEntity.ok(equipo);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Integer id) {
+        equipoService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
