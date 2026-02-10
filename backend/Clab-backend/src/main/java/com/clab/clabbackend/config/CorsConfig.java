33c78971c6dfdfbc16374ce4a2825759c2b6531d
BYRONDANIELLOORMENDO <bloorm2@uteq.edu.ec>
2026-02-10 15:01:56 -0500

    USUARIOS COMPLETOS, CON TOKEN Y TODO Y EL USERNAME ES byron.loor y las password es 1234

--- backend/Clab-backend/src/main/java/com/clab/clabbackend/config/CorsConfig.java
+++ backend/Clab-backend/src/main/java/com/clab/clabbackend/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.clab.clabbackend.confi;
+package com.clab.clabbackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
